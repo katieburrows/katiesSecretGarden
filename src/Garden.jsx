c51067e71bcc3160355c56a2a4b861ca2ff0b34b
Ana Perez-Santos <anaperez-santos@Anas-MacBook-Pro-2.local>
2021-11-01 16:50:41 -0600

    Build add form functionality

--- src/Garden.jsx
+++ src/Garden.jsx
@@ -6,45 +6,45 @@ const data = [
   {
     plantName: "snow pea",
     type: "annual",
-    isWatered: true,
     numberPlanted: 5,
     hoursOfSunlight: 8,
+    isWatered: true,
     isHealthy: true,
     isAlive: true
   },
   {
     plantName: "bell pepper",
     type: "perennial",
-    isWatered: false,
     numberPlanted: 15,
     hoursOfSunlight: 6,
+    isWatered: false,
     isHealthy: false,
     isAlive: true
   },
   {
     plantName: "onion",
     type: "biennial",
-    isWatered: true,
     numberPlanted: 108,
     hoursOfSunlight: 13,
+    isWatered: true,
     isHealthy: true,
     isAlive: true
   },
   {
     plantName: "cucumber",
     type: "annual",
-    isWatered: false,
     numberPlanted: 1,
     hoursOfSunlight: 9,
+    isWatered: false,
     isHealthy: false,
     isAlive: false
   },
   {
     plantName: "lettuce",
     type: "annual",
-    isWatered: true,
     numberPlanted: 5,
     hoursOfSunlight: 8,
+    isWatered: true,
     isHealthy: false,
     isAlive: false
   },
@@ -52,21 +52,29 @@ const data = [
 
 const Garden = () => {
   const [veg, setVeg] = useState(data);
+  
+  //To add a new plant, we created this method that takes in a plantObject as an argument, passes into the veg state method using a spread operator, and adds it to the top
+  const addVeg = function(plantObject) {
+    setVeg([plantObject, ...veg])
+    //console.log(plantObject);
+  }
+
   const deleteVeg = function(plantName) {
     const remainingVeg = veg.filter(item => item.plantName !== plantName)
     setVeg(remainingVeg);
   }
+
   return (
     <>
       <ul>
-        {veg.map(({ plantName, type, isWatered, numberPlanted, hoursOfSunlight, isHealthy, isAlive }) => (
+        {veg.map(({ plantName, type, numberPlanted, hoursOfSunlight, isWatered, isHealthy, isAlive }) => (
           <Plant
             key={plantName}
             plantName={plantName}
             type={type}
-            isWatered={isWatered}
             numberPlanted={numberPlanted}
             hoursOfSunlight={hoursOfSunlight}
+            isWatered={isWatered}
             isHealthy={isHealthy}
             isAlive={isAlive}
             deleteVeg={deleteVeg}
@@ -74,7 +82,10 @@ const Garden = () => {
         ))}
       </ul>
 
-      <Form />
+      <Form
+      //Similar to giving the Plant component access to the deleteVeg method, we gave the Form component access to the addVeg method, because that is where the user input is
+        addVeg={addVeg}
+      />
     </>
   );
 };
